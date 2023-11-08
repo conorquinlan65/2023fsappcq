7a8180724a46a66fd643f9861f4efd28f740978c
Conor Quinlan <conor.quinlan@du.edu>
2023-11-08 10:22:57 -0700

    Day 2 - Updating Registration Form, Comments added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,19 +16,44 @@ const Login = () => {
                 <div className="underline"></div>
             </div>
             <div className="inputs">
-                {action === "Login"?<div></div>: <div className="input">
+                /** When in the Registration Page, display more input fields to allow for:
+                 * - a Name
+                 * - a Username
+                 * - a DOB
+                 */
+                {action === "Login"?<div></div>: 
+                <div className="input">
                     <img src={user_icon} alt=""></img>
                     <input type="text" placeholder="Name"></input>
+                </div>
+                /** This would need to be fixed to have a different ICON */
+                <div className="input">
+                    <img src={user_icon} alt=""></img>
+                    <input type="text" placeholder="Username"></input>
+                </div>
+                /** This would need to be fixed to have DOB be a LIST */
+                <div className="input">
+                    <img src={user_icon} alt=""></img>
+                    <input type="text" placeholder="DOB"></input>
                 </div>}
+                /** When in the Login Page, display less input fields to allow for:
+                 * - a Email
+                 * - a Password
+                 */
+
+                 /** Input for a Email ID wrapped in a input, image, input, placeholder */
                 <div className="input">
                     <img src={email_icon} alt=""></img>
                     <input type="email" placeholder="Email Id"></input>
                 </div>
+                /** Input for a Password wrapped in a input, image, input, placeholder */
                 <div className="input">
                     <img src={pass_icon} alt=""></img>
                     <input type="password" placeholder="Password"></input>
                 </div>
-                {action==="Sign Up"?<div></div>:<div className="forgot-password"> Lost Password? <span>Click Here!</span></div>}
+                /** This is what enables the Sign Up link page */
+                {action==="Sign Up"?<div></div>:
+                <div className="forgot-password"> Lost Password? <span>Click Here!</span></div>}
                 <div>
                     <div className="submit-container">
                         <div className={action === "Sign Up"?"submit gray":"submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
